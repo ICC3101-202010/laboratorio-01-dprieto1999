90ef3eca69d910d84d30090172d7c40a8e915101
unknown <dprieto@miuandes.cl>
2020-03-23 19:46:23 -0300

    commit 1

---
+++ laboratorio1/laboratorio1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace laboratorio1
+{//cachipun
+    class Program
+    {
+        static void Main(string[] args)
+        {//constructor
+            Persona jugador = new Persona("Bob", "Kunga");
+            jugador.lanzar();
+        }
+        class Persona
+        {
+            //atributos
+            private string Nombre;
+            private string Apellido;
+            public Persona(string name, string lastname)
+            {
+            Nombre = name;
+            Apellido = lastname;
+            }
+            //metodo
+            public void lanzar()
+            {
+                Random random = new Random();
+                Console.WriteLine(random.Next(0, 3));
+            }
+        }
+    }
+}
